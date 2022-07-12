8e815a054e1978a253e1a9468044cdb72bd12e4a
tatokhorava <tato.khorava@gmail.com>
2022-07-12 12:05:10 +0400

    add JobApplicationService

---
+++ Ara.Domain/ApplicationServices/JobApplicationService.cs
@@ -0,0 +1,22 @@
+﻿using Ara.Domain.Dtos;
+using Ara.Domain.JobManagement;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ara.Domain.ApplicationServices
+{
+    public class JobApplicationService
+    {
+        public List<JobListItemDto> GetJobs()
+        {
+            return new List<JobListItemDto>();
+        }
+
+
+        public Job GetJob(int id)
+        {
+            return new Job();
+        }
+    }
+}
