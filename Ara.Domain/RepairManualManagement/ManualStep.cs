4dc6ea2e7d8ae71db212637a2046e8249c7c4a81
tatokhorava <tato.khorava@gmail.com>
2022-07-18 16:41:41 +0400

    manual sample data completed

--- Ara.Domain/RepairManualManagement/ManualStep.cs
+++ Ara.Domain/RepairManualManagement/ManualStep.cs
@@ -11,10 +11,17 @@ namespace Ara.Domain.RepairManualManagement
         public string Title { get; set; }
         public string Description { get; set; }
         public List<Message> Messages { get; set; }
-        public List<string> Photos { get; set; }
+        public List<ManualStepPhoto> Photos { get; set; }
         public List<string> Labels { get; set; }
         public RepairManual ReferencedManual { get; set; }
         public int? ReferencedManualId { get; set; }
         public List<ManualStep> SubSteps { get; set; }
     }
+
+    public class ManualStepPhoto
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+
 }
