5248b1ed403a77779e066fdd15e3a4c5bd872564
GoodWink <PatricioGameDev@gmail.com>
2022-10-20 12:21:25 -0400

    Purge

--- ara_app/Assets/BoxOfClicks/BTagged/Scripts/BTaggedGroup.cs
+++
@@ -1,6 +0,0 @@
-﻿/* Copyright(c) Tim Watts, Box of Clicks - All Rights Reserved */
-
-namespace BOC.BTagged
-{
-    public class BTaggedGroup : BTaggedGroup<BTaggedSO> {}
-}
--- ara_app/Assets/BoxOfClicks/BTagged/Scripts/Tag/TagGroup.cs
+++
@@ -1,9 +0,0 @@
-﻿/* Copyright(c) Tim Watts, Box of Clicks - All Rights Reserved */
-
-using UnityEngine;
-
-namespace BOC.BTagged
-{
-    [CreateAssetMenu(fileName = "TagGroup", menuName = "BTagged/Tag Group")]
-    public class TagGroup : BTaggedGroup<Tag>{}
-}
