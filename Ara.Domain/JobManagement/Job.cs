a93286b8691664cd0a9bc3694958d468f1ed55b0
tkhoravaDC <73892458+tkhoravaDC@users.noreply.github.com>
2022-11-01 02:29:26 +0400

    Refactoring/add seperate dll (#6)
    * test
    
    * Bugfix - Ara.Domain dll reference fixed
    
    Co-authored-by: tatokhorava <tato.khorava@gmail.com>

---
+++ Ara.Domain/JobManagement/Job.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ara.Domain.JobManagement
+{
+    public class Job
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Code { get; set; }
+        public decimal Progress { get; set; }
+        public JobStatus Status { get; set; }
+        public Car Car { get; set; }
+        public CarOwner CarOwner { get; set; }
+        public List<TaskInfo> Tasks { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string CreatedBy { get; set; }
+        public List<string> InitialPhotos { get; set; }
+
+        public enum JobStatus
+        {
+            ToDo = 1,
+            InProgress = 2,
+            Completed = 3
+        }
+
+    }
+}
