6122448df2523a3bdc777b8186d10141b5f9eacc
tatokhorava <tato.khorava@gmail.com>
2022-11-14 18:02:52 +0400

    sqlLite dll fixed

--- ARA_MRTK3/Assets/Scripts/Infrastructure/SqlLiteDataProvider.cs
+++ ARA_MRTK3/Assets/Scripts/Infrastructure/SqlLiteDataProvider.cs
@@ -1,35 +1,38 @@
-﻿//using Ara.Domain.Repositories;
+﻿
 //using Mono.Data.Sqlite;
-//using System;
-//using System.Collections.Generic;
-//using System.Data;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+using System.Data;
 
-//namespace Assets.Scripts.Infrastructure
-//{
-//    public class SqlLiteDataProvider : IDataProvider
-//    {
-//        public IDbConnection Db { get; set; }
-//        public SqlLiteDataProvider()
-//        {
-//            this.Db = this.CreateAndOpenDatabase();
-//        }
+using Ara.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mono.Data.Sqlite;
 
-//        private IDbConnection CreateAndOpenDatabase()
-//        {
-//            // Open a connection to the database.
-//            string dbUri = "URI=file:AraDatabase.sqlite";
-//            IDbConnection dbConnection = new SqliteConnection(dbUri);
-//            dbConnection.Open();
+namespace Assets.Scripts.Infrastructure
+{
+    public class SqlLiteDataProvider : IDataProvider
+    {
+        public IDbConnection Db { get; set; }
+        public SqlLiteDataProvider()
+        {
+            this.Db = this.CreateAndOpenDatabase();
+        }
 
-//            // Create a table for the hit count in the database if it does not exist yet.
-//            IDbCommand dbCommandCreateTable = dbConnection.CreateCommand();
-//            dbCommandCreateTable.CommandText = "CREATE TABLE IF NOT EXISTS TaskPhotos (id UNIQUEIDENTIFIER PRIMARY KEY, taskId INTEGER, createdOn datetime, createdBy INTEGER, IsSynchronized BIT, synchronizedOn)";
-//            dbCommandCreateTable.ExecuteReader();
+        private IDbConnection CreateAndOpenDatabase()
+        {
+            // Open a connection to the database.
+            string dbUri = "URI=file:AraDatabase.sqlite";
+            IDbConnection dbConnection = new SqliteConnection(dbUri);
+            dbConnection.Open();
 
-//            return dbConnection;
-//        }
-//    }
-//}
+            // Create a table for the hit count in the database if it does not exist yet.
+            IDbCommand dbCommandCreateTable = dbConnection.CreateCommand();
+            dbCommandCreateTable.CommandText = "CREATE TABLE IF NOT EXISTS TaskPhotos (id UNIQUEIDENTIFIER PRIMARY KEY, taskId INTEGER, createdOn datetime, createdBy INTEGER, IsSynchronized BIT, synchronizedOn)";
+            dbCommandCreateTable.ExecuteReader();
+
+            return dbConnection;
+        }
+    }
+}
