cfa9a36fec763da90165b7791e36107ab87b8015
tatokhorava <tato.khorava@gmail.com>
2022-11-15 17:33:56 +0400

    test project added

---
+++ Ara.Domain.Test/ApplicationEventDispatcherTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Ara.Domain.Test
+{
+    public class ApplicationEventDispatcherTests
+    {
+        [Fact]
+        public void AddEvent_ShouldAddHandlerToList_When()
+        {
+            Assert.Fail("failed");
+        }
+    }
+}
