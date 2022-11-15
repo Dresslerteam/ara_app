1f270f72e7093035cf228cd8274a25fc22a2de80
tatokhorava <tato.khorava@gmail.com>
2022-11-16 01:10:48 +0400

    Add new test

--- Ara.Domain.Test/ApplicationEventDispatcherTests.cs
+++ Ara.Domain.Test/ApplicationEventDispatcherTests.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Ara.Domain.Common.Interfaces;
+using Ara.Domain.Common.Services;
+using Ara.Domain.Handlers.EventHandlers;
+using Ara.Domain.Handlers.Events;
+using NSubstitute;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,9 +15,18 @@ namespace Ara.Domain.Test
     public class ApplicationEventDispatcherTests
     {
         [Fact]
-        public void AddEvent_ShouldAddHandlerToList_When()
+        public void Dispatch_ShouldCallHandle_WhenHandlerAdded()
         {
-            Assert.Fail("failed");
+            ApplicationEventDispatcher applicationEventDispatcher = new ApplicationEventDispatcher();
+            var testEvent = new PhotoSavedEvent();
+            var testEventHandler = Substitute.For<IEventHandler<PhotoSavedEvent>>();
+
+            applicationEventDispatcher.AddListener(testEventHandler);
+
+            applicationEventDispatcher.Dispatch(testEvent);
+
+            testEventHandler.Received(1).Handle(testEvent);
+            Assert.True(true);
         }
     }
 }
