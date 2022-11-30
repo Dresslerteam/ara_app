52dd93c88a4661bb855e073abcfce3b4ac2d6b32
tkhoravaDC <73892458+tkhoravaDC@users.noreply.github.com>
2022-11-30 12:08:52 +0400

    Feature/ara 61 track user actions (#11)
    * test project added
    
    * remove bin from git
    
    * Add new test
    
    Co-authored-by: tatokhorava <tato.khorava@gmail.com>

---
+++ Ara.Domain.Test/ApplicationEventDispatcherTests.cs
@@ -0,0 +1,32 @@
+﻿using Ara.Domain.Common.Interfaces;
+using Ara.Domain.Common.Services;
+using Ara.Domain.Handlers.EventHandlers;
+using Ara.Domain.Handlers.Events;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Ara.Domain.Test
+{
+    public class ApplicationEventDispatcherTests
+    {
+        [Fact]
+        public void Dispatch_ShouldCallHandle_WhenHandlerAdded()
+        {
+            ApplicationEventDispatcher applicationEventDispatcher = new ApplicationEventDispatcher();
+            var testEvent = new PhotoSavedEvent();
+            var testEventHandler = Substitute.For<IEventHandler<PhotoSavedEvent>>();
+
+            applicationEventDispatcher.AddListener(testEventHandler);
+
+            applicationEventDispatcher.Dispatch(testEvent);
+
+            testEventHandler.Received(1).Handle(testEvent);
+            Assert.True(true);
+        }
+    }
+}
