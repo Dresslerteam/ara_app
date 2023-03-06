1833159fc0074d02a045b65d706561f7c2eecdc9
tatokhorava <tato.khorava@gmail.com>
2023-03-07 00:49:24 +0400

    Add tests for UserService

---
+++ Ara.Domain.Test/UserServiceTest.cs
@@ -0,0 +1,33 @@
+﻿using Ara.Domain.ApplicationServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Ara.Domain.Test
+{
+    public class UserServiceTest
+    {
+        [Fact]
+        public async void GetUsers_Successfully()
+        {
+            var userService = new UserService();
+            var users = userService.GetAllUsers();
+            Assert.True(true);
+
+            Assert.True(userService.GetCurrentUserInfo() == null);
+        }
+
+        [Fact]
+        public async void Login_Successfully()
+        {
+            var userService = new UserService();
+            userService.Login("tato@khorava.com", null);
+            var currentUser = userService.GetCurrentUserInfo();
+            Assert.True(currentUser != null);
+            Assert.True(currentUser.Email == "tato@khorava.com");
+        }
+    }
+}
