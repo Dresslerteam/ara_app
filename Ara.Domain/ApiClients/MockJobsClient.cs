3a9fb4391fb96e5c95b62d244c3d5541827481de
tatokhorava <tato.khorava@gmail.com>
2023-03-11 14:51:17 +0400

    add data for steps.. to Front Bumper - Removal

--- Ara.Domain/ApiClients/MockJobsClient.cs
+++ Ara.Domain/ApiClients/MockJobsClient.cs
@@ -41,12 +41,334 @@ namespace Ara.Domain.ApiClients
                     new TaskInfo()
                     {
                         Id = 1,
-                        Title = "Front Bumper - Removal"
+                        Title = "Front Bumper - Removal",
+                        Group = "Front Bumper",
+                        SequenceNumber =1,
+                        Status = TaskInfo.TaskStatus.ToDo,
+                         RepairManuals = new List<RepairManual>()
+                        {
+                            new RepairManual()
+                            {
+                                Id = "1",
+                                Name = "Front Bumper Impact Bar Removal and Installation",
+                                Document = new PdfDoc()
+                                {
+                                    Title= "Front Bumper Impact Bar Removal and Installation",
+                                    Url = "Front_Bumper_Impact_Bar_Removal_and_Installation.pdf"
+                                },
+                                Steps = new List<ManualStep>()
+                                {
+                                    new ManualStep()
+                                    {
+                                        Id = 1,
+                                        Title = "Hood Latch Actuator Bolt (1) >> Remove [2x] (1)",
+                                        Image = new ManualStep.StepImage
+                                        {
+                                            Title = "1.png",
+                                            Url = "1.png"
+                                        },
+                                        PhotoRequired = true
+                                    },
+                                    new ManualStep()
+                                    {
+                                        Id = 2,
+                                        Title = "Hood Secondary Latch Release Handle (2) » Remove",
+                                        Image = new ManualStep.StepImage
+                                        {
+                                            Title = "1.png",
+                                            Url = "1.png"
+                                        },
+                                        PhotoRequired = false
+                                    },
+                                    new ManualStep()
+                                    {
+                                        Id = 3,
+                                        Title = "Intake Air Baffle Retainer (1) » Remove [10x]",
+                                        Image = new ManualStep.StepImage
+                                        {
+                                            Title = "2.png",
+                                            Url = "2.png"
+                                        },
+                                        PhotoRequired = true
+                                    },
+                                    new ManualStep()
+                                    {
+                                        Id = 4,
+                                        Title = "Intake Air Splash Shield (2) » Remove",
+                                        Image = new ManualStep.StepImage
+                                        {
+                                            Title = "2.png",
+                                            Url = "2.png"
+                                        },
+                                        PhotoRequired = true
+                                    },
+                                    new ManualStep()
+                                    {
+                                        Id = 5,
+                                        Title = "Front Grille Bolt (1) » Remove [4x]",
+                                        Image = new ManualStep.StepImage
+                                        {
+                                            Title = "3.png",
+                                            Url = "3.png"
+                                        },
+                                        PhotoRequired = false
+                                    },
+                                    new ManualStep()
+                                    {
+                                        Id = 6,
+                                        Title = "Front Grille Retainer (1) » Release [8x]",
+                                        Image = new ManualStep.StepImage
+                                        {
+                                            Title = "4.png",
+                                            Url = "4.png"
+                                        },
+                                        PhotoRequired = false
+                                    },
+                                    new ManualStep()
+                                    {
+                                        Id = 7,
+                                        Title = "Front Grille Clip (2) » Release [8x]",
+                                        Image = new ManualStep.StepImage
+                                        {
+                                            Title = "4.png",
+                                            Url = "4.png"
+                                        },
+                                        PhotoRequired = true
+                                    },
+                                    new ManualStep()
+                                    {
+                                        Id = 8,
+                                        Title = "Front Grille (3) » Remove",
+                                        Image = new ManualStep.StepImage
+                                        {
+                                            Title = "4.png",
+                                            Url = "4.png"
+                                        },
+                                        PhotoRequired = true
+                                    },
+                                    new ManualStep()
+                                    {
+                                        Id = 9,
+                                        Title = "{ If equipped } Disconnect and reposition the lower shutter harness.",
+                                        Image = new ManualStep.StepImage
+                                        {
+                                            Title = "5.png",
+                                            Url = "5.png"
+                                        },
+                                        PhotoRequired = true
+                                    },
+                                    new ManualStep()
+                                    {
+                                        Id = 10,
+                                        Title = "Disconnect the electrical connector.",
+                                        Image = new ManualStep.StepImage
+                                        {
+                                            Title = "5.png",
+                                            Url = "5.png"
+                                        },
+                                        PhotoRequired = true
+                                    },
+                                     new ManualStep()
+                                    {
+                                        Id = 11,
+                                        Title = "Front Bumper Shutter Bolt (1) » Remove [9x]",
+                                        Image = new ManualStep.StepImage
+                                        {
+                                            Title = "5.png",
+                                            Url = "5.png"
+                                        },
+                                        PhotoRequired = true
+                                    },
+                                      new ManualStep()
+                                    {
+                                        Id = 12,
+                                        Title = "Front Bumper Shutter (2) » Remove",
+                                        Image = new ManualStep.StepImage
+                                        {
+                                            Title = "5.png",
+                                            Url = "5.png"
+                                        },
+                                        PhotoRequired = true
+                                    },
+                                       new ManualStep()
+                                    {
+                                        Id = 13,
+                                        Title = "Front Bumper Impact Bar Bolt (1) » Remove [4x]",
+                                        Image = new ManualStep.StepImage
+                                        {
+                                            Title = "6.png",
+                                            Url = "6.png"
+                                        },
+                                        PhotoRequired = true
+                                    },
+                                        new ManualStep()
+                                    {
+                                        Id = 14,
+                                        Title = "Remove the left and right front bumper fascia support brace bolts (1)",
+                                        Image = new ManualStep.StepImage
+                                        {
+                                            Title = "14.png",
+                                            Url = "14.png"
+                                        },
+                                        PhotoRequired = true
+                                    },
+                                        new ManualStep()
+                                    {
+                                        Id = 15,
+                                        Title = "Remove the 2 left and right front bumper fascia center support bolts (2)",
+                                        Image = new ManualStep.StepImage
+                                        {
+                                            Title = "14.png",
+                                            Url = "14.png"
+                                        },
+                                        PhotoRequired = true
+                                    },
+                                        new ManualStep()
+                                    {
+                                        Id = 16,
+                                        Title = "With the aid of an assistant remove the fascia (1).",
+                                        Image = new ManualStep.StepImage
+                                        {
+                                            Title = "16.png",
+                                            Url = "16.png"
+                                        },
+                                        PhotoRequired = false
+                                    },
+                                        new ManualStep()
+                                    {
+                                        Id = 17,
+                                        Title = "Disconnect electrical connections, if applicable.",
+                                        Image = new ManualStep.StepImage
+                                        {
+                                            Title = "16.png",
+                                            Url = "16.png"
+                                        },
+                                        PhotoRequired = true
+                                    }
+                                }
+                            },
+                            new RepairManual()
+                            {
+                                Id = "2",
+                                Name = "Front Bumper Impact Bar Adjustment",
+                                Document = new PdfDoc()
+                                {
+                                    Title= "Front Bumper Impact Bar Adjustment",
+                                    Url = "Front_Bumper_Impact_Bar_Adjustment_Document_ID_5633756.pdf"
+                                },
+                                Steps = new List<ManualStep>()
+                                {
+                                    new ManualStep()
+                                    {
+                                        Id = 18,
+                                        Title = "Measure and document the impact bar to headlamps dimensions and the impact bar to fenders dimensions and note how far to move the impact bar right, left, up, and down to achieve an equal gap to the fenders and headlamps.",
+                                        Image = null, //todo:what if there is not step photo ? 
+                                        PhotoRequired = false
+                                    },
+                                    new ManualStep()
+                                    {
+                                        Id = 19,
+                                        Title = "Front Bumper Impact Bar (1) » Remove",
+                                        Image = new ManualStep.StepImage
+                                        {
+                                            Title = "19.png",
+                                            Url = "19.png"
+                                        },
+                                        PhotoRequired = false,
+
+                                    },
+                                     new ManualStep()
+                                    {
+                                        Id = 20,
+                                        Title = "With a grease pencil, mark the top and side bumper adjustment bracket (2).",
+                                        Image = new ManualStep.StepImage
+                                        {
+                                            Title = "20.png",
+                                            Url = "20.png"
+                                        },
+                                        PhotoRequired = false
+                                    },
+                                     new ManualStep()
+                                    {
+                                        Id = 21,
+                                        Title = "Loosen the 6 impact bar adjustment bracket bolts (1) and move the 2 adjustment brackets (2) to the pre determined measurements taken",
+                                        Image = new ManualStep.StepImage
+                                        {
+                                            Title = "20.png",
+                                            Url = "20.png"
+                                        },
+                                        PhotoRequired = false
+                                    }
+                                }
+                            },
+                            new RepairManual()
+                            {
+                                Id = "3",
+                                Name = "Front Bumper Impact Bar Bracket Replacement",
+                                Document = new PdfDoc()
+                                {
+                                    Title= "Front Bumper Impact Bar Bracket Replacement",
+                                    Url = "Front_Bumper_Impact_Bar_Bracket_Replacement.pdf"
+                                },
+                                Steps = new List<ManualStep>()
+                                {
+                                    new ManualStep()
+                                    {
+                                        Id = 22,
+                                        Title = "Front Bumper Impact Bar (1) » Remove ",
+                                        Image = new ManualStep.StepImage
+                                        {
+                                            Title = "22.png",
+                                            Url = "22.png"
+                                        },
+                                        PhotoRequired = false
+                                    },
+                                    new ManualStep()
+                                    {
+                                        Id = 23,
+                                        Title = "{ If equipped } Radiator Air Lower Baffle Bolt (1) » Remove [2x]",
+                                        Image = new ManualStep.StepImage
+                                        {
+                                            Title = "23.png",
+                                            Url = "23.png"
+                                        },
+                                        PhotoRequired = false,
+
+                                    },
+                                     new ManualStep()
+                                    {
+                                        Id = 24,
+                                        Title = "Front Bumper Impact Bar Bracket Bolt (1) » Remove [3x]",
+                                        Image = new ManualStep.StepImage
+                                        {
+                                            Title = "24.png",
+                                            Url = "24.png"
+                                        },
+                                        PhotoRequired = false
+                                    },
+                                     new ManualStep()
+                                    {
+                                        Id = 24,
+                                        Title = "Front Bumper Impact Bar Bracket (2) » Remove",
+                                        Image = new ManualStep.StepImage
+                                        {
+                                            Title = "24.png",
+                                            Url = "24.png"
+                                        },
+                                        PhotoRequired = false
+                                    }
+                                }
+                            }
+                        }
                     },
                      new TaskInfo()
                     {
                         Id = 2,
-                        Title = "Front Bumper - Installation"
+                        Title = "Front Bumper - Installation",
+                        Group = "Front Bumper",
+                        SequenceNumber = 1,
+                        Status = TaskInfo.TaskStatus.ToDo,
+                       
                     }
                 },
                 PreliminaryEstimation = new PdfDoc()
