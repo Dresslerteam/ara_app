ed35eff35249ad4f8975a6e34f9117053322f4aa
Cardboard <38846947+SugarSores@users.noreply.github.com>
2023-03-22 20:26:19 -0400

    Good news, it doesn't break anything
    Bad news, Scrolling still doesn't work, not even their examples.

--- ARA_MRTK3/Assets/Scripts/Ruler/TwoHandsRuler.cs
+++ ARA_MRTK3/Assets/Scripts/Ruler/TwoHandsRuler.cs
@@ -1,7 +1,6 @@
 using Microsoft.MixedReality.Toolkit;
 using Microsoft.MixedReality.Toolkit.Input;
 using Microsoft.MixedReality.Toolkit.Subsystems;
-using Microsoft.MixedReality.Toolkit.Utilities;
 using TMPro;
 using UnityEngine;
 using UnityEngine.XR;
