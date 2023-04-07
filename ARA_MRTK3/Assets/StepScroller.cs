9015da5bfd983c06f65a38acc553adfdb01da9c9
Cardboard <38846947+SugarSores@users.noreply.github.com>
2023-04-06 21:06:42 -0400

    Step Scrolling (could be more dynamic, but ok), also fixed side menu

---
+++ ARA_MRTK3/Assets/StepScroller.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.MixedReality.Toolkit.UX;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StepScroller : MonoBehaviour
+{
+    [SerializeField] private float minY;
+    [SerializeField] private float maxY;
+    [SerializeField] private RectTransform content;
+    
+    public void UpdateFromSliderEventData(SliderEventData sliderEventData)
+    {
+        NormalizedScroll(sliderEventData.NewValue);
+    }
+    public void NormalizedScroll(float normalizedValue)
+    {
+        float y = Mathf.Lerp(minY, maxY, normalizedValue);
+        content.anchoredPosition = new Vector2(content.anchoredPosition.x, y);
+    }
+    
+    public void SetMaxY(float maxY)
+    {
+        this.maxY = maxY;
+    }
+}
