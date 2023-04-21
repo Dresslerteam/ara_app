9d0abd5b5c5b30c6cbd9f562b4c0ea6c835a3f34
tkhoravaDC <73892458+tkhoravaDC@users.noreply.github.com>
2023-04-21 21:57:30 +0400

    Bugfix/add_uncomplete_confirmation_modal (#33)
    * make sure on first enterings steps screen to hide all buttons
    
    * unCompleteButton Confirmation modal added
    
    * fix merge conflicts
    
    ---------
    
    Co-authored-by: tatokhorava <tato.khorava@gmail.com>

--- ARA_MRTK3/Assets/Scripts/WorkingHUDManager.cs
+++ ARA_MRTK3/Assets/Scripts/WorkingHUDManager.cs
@@ -6,6 +6,7 @@ using Ara.Domain.ApiClients.Dtos;
 using Ara.Domain.Common;
 using Ara.Domain.JobManagement;
 using Ara.Domain.RepairManualManagement;
+using Assets.Scripts;
 using Assets.Scripts.Common;
 using Microsoft.MixedReality.Toolkit;
 using Microsoft.MixedReality.Toolkit.UX;
@@ -39,6 +40,7 @@ public class WorkingHUDManager : MonoBehaviour
     [SerializeField] private PressableButton completeButton;
     [SerializeField] private PressableButton unCompleteButton;
     [SerializeField] public GameObject photoRequiredModal;
+    [SerializeField] public UnCompleteStepConfirmationModal unCompleteStepConfirmationModal;
     [SerializeField][AssetsOnly] private PressableButton cautionPdfButtonPrefab;
     [SerializeField][AssetsOnly] private PressableButton oemPdfButtonPrefab;
     public GameObject takePicture;
@@ -53,6 +55,11 @@ public class WorkingHUDManager : MonoBehaviour
     private Dictionary<int, StepDisplay> _stepDisplays = new Dictionary<int, StepDisplay>();
     public void PopulateTaskGroups(TaskInfo task)
     {
+        foreach (Transform child in buttonsRoot)
+        {
+            Destroy(child.gameObject);
+        }
+        manualButtonCollection.Toggles.Clear();
         _stepDisplays = new Dictionary<int, StepDisplay>();
         _repairManualDisplays = new Dictionary<int, RepairManualDisplay>();
         currentTask = task;
@@ -329,10 +336,19 @@ public class WorkingHUDManager : MonoBehaviour
         {
             if (step.IsCompleted)
             {
-                step.IsCompleted = false;
-                unCompleteButton.gameObject.SetActive(false);
-                completeButton.gameObject.SetActive(true);
-                stepDisplay.UnCompleteStep();
+                unCompleteStepConfirmationModal.Show(
+                    confirmationCallback: () =>
+                    {
+                        step.IsCompleted = false;
+                        unCompleteButton.gameObject.SetActive(false);
+                        completeButton.gameObject.SetActive(true);
+                        stepDisplay.UnCompleteStep();
+                    },
+
+                    cancellationCallback: () =>
+                    {
+                        unCompleteStepConfirmationModal.Hide();
+                    });
             }
 
         });
