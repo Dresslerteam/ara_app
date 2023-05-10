8b00ff255b6285ebaabdd4b4193ea485f25c5b06
josh-Dressler <131477533+josh-Dressler@users.noreply.github.com>
2023-05-10 11:06:35 -0400

    Feature/ StepSliderUsabilty (#43)
    * Updates slider to use unity UI rect scroll
    still some odd things about it so I still needed some of our scroller ui code
    
    enlargened slider bar
    User can click and drag on the entire scroll rect now
    
    * Refactor Task List in WorkHud
    - Lots of things pulled from workhudpage and into stepListcontroller, RepairManualDisplay and StepDisplay.
    - proper sizing of viewport
    - button colliders activation fixed for initiating, expanding and movement
    - Refactored a lot of things down from workHud as well as up from StepDisplay and RepariManuals into StepListCont for optimizing like UpdateColliders
    
    * Added new MRTK WIP Script Scrollable
    This allows some scroll rect functionality with MRTK
    Using this i was able to make the scroll bar a draggable object
    
    * fix for MRTK edited file "ToggleCollection"
    
    * Fixed some graphics and sizing issues
    
    * fixed slider rotation bug
    
    * Pull changes out of mrtk package and into scripts that commit to repo

--- ARA_MRTK3/Assets/Scripts/StepDisplay.cs
+++ ARA_MRTK3/Assets/Scripts/StepDisplay.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StepDisplay : MonoBehaviour
 {
@@ -13,6 +14,29 @@ public class StepDisplay : MonoBehaviour
     [SerializeField] private GameObject stepCompleteIcon;
 
     public Action<bool> OnStepChange;
+
+    private RectTransform rect;
+    private BoxCollider collider;
+    private RectTransform scrollerRect;
+    private ScrollRect _scrollRect;
+    private StepScroller _stepScroller;
+
+    private void Start()
+    {
+        _scrollRect = GetComponentInParent<ScrollRect>();
+
+        scrollerRect = _scrollRect.GetComponent<RectTransform>();
+
+        rect = GetComponent<RectTransform>();
+
+        collider = GetComponent<BoxCollider>();
+
+        _stepScroller = GetComponentInParent<StepScroller>();
+
+        if (_stepScroller != null) _stepScroller.OnMove += UpdateColliders;
+        if (_scrollRect != null) _scrollRect.onValueChanged.AddListener((Vector2 value) => { UpdateColliders(); });
+
+    }
 
     public void UpdateDisplayInformation(int index, string text, bool isComplete, Transform parent)
     {
@@ -38,4 +62,31 @@ public class StepDisplay : MonoBehaviour
 
     }
 
+    public void UpdateColliders()
+    {
+        if (scrollerRect == null || collider == null || rect == null) return;
+
+        Vector3[] corners = new Vector3[4];
+
+        scrollerRect.GetWorldCorners(corners);
+
+        float top = 0;
+
+        float bottom = float.MaxValue;
+
+        foreach (Vector3 corner in corners)
+        {
+            if (top < corner.y) top = corner.y;
+
+            if (bottom > corner.y) bottom = corner.y;
+        }
+
+
+        bool enabled =
+            top > (rect.position.y) &&
+          bottom < (rect.position.y);
+
+        collider.enabled = enabled;
+
+    }
 }
